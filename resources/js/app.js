88714fb44fdf847a1370d73bee69ea26b42df3ca
mohaimen <mohaimen707@gmail.com>
2020-09-03 00:47:59 +0600

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+/*window.Vue = require('vue');*/
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app',
+});
